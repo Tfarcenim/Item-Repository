1e52b4bcd975034cfa7d89d6322cdb93b587b89a
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2022-11-07 23:04:14 -0500

    texture fixes

--- src/main/java/tfar/itemrepository/client/Client.java
+++ src/main/java/tfar/itemrepository/client/Client.java
@@ -1,9 +1,12 @@
 package tfar.itemrepository.client;
 
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import tfar.itemrepository.init.ModBlockEntityTypes;
+import tfar.itemrepository.init.ModBlocks;
 import tfar.itemrepository.init.ModMenuTypes;
 
 public class Client {
@@ -11,5 +14,10 @@ public class Client {
     public static void setup(FMLClientSetupEvent e) {
         MenuScreens.register(ModMenuTypes.REPOSITORY,RepositoryScreen::new);
         BlockEntityRenderers.register(ModBlockEntityTypes.BETTER_BARREL,BetterBarrelRenderer::new);
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.BETTER_BARREL, RenderType.cutoutMipped());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.STONE_BETTER_BARREL, RenderType.cutoutMipped());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.COPPER_BETTER_BARREL, RenderType.cutoutMipped());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.IRON_BETTER_BARREL, RenderType.cutoutMipped());
+
     }
 }
