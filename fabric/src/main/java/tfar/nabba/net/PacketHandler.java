1742a1ad4f4e9a4a96fc91640bd6347ba13dacf2
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-10-03 13:33:37 -0400

    Update PacketHandler.java

--- fabric/src/main/java/tfar/nabba/net/PacketHandler.java
+++ fabric/src/main/java/tfar/nabba/net/PacketHandler.java
@@ -11,6 +11,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
 import tfar.nabba.NABBA;
 import tfar.nabba.item.keys.KeyRingItem;
 import tfar.nabba.menu.SearchableFluidMenu;
@@ -26,28 +27,28 @@ import java.util.function.Consumer;
 
 public class PacketHandler {
 
-    public static final ResourceLocation scroll = new ResourceLocation(NABBA.MODID,"scroll");
-    public static final ResourceLocation scroll_keyring = new ResourceLocation(NABBA.MODID,"scroll_keyring");
-    public static final ResourceLocation search = new ResourceLocation(NABBA.MODID,"search");
-    public static final ResourceLocation vanity = new ResourceLocation(NABBA.MODID,"vanity");
-    public static final ResourceLocation extract_item = new ResourceLocation(NABBA.MODID,"extract_item");
-    public static final ResourceLocation extract_fluid = new ResourceLocation(NABBA.MODID,"extract_fluid");
-    public static final ResourceLocation force_sync = new ResourceLocation(NABBA.MODID,"force_sync");
-    public static final ResourceLocation insert = new ResourceLocation(NABBA.MODID,"insert");
-    public static final ResourceLocation refresh_items = new ResourceLocation(NABBA.MODID,"refresh_items");
-    public static final ResourceLocation refresh_fluids = new ResourceLocation(NABBA.MODID,"refresh_fluids");
+    public static final ResourceLocation scroll = new ResourceLocation(NABBA.MODID, "scroll");
+    public static final ResourceLocation scroll_keyring = new ResourceLocation(NABBA.MODID, "scroll_keyring");
+    public static final ResourceLocation search = new ResourceLocation(NABBA.MODID, "search");
+    public static final ResourceLocation vanity = new ResourceLocation(NABBA.MODID, "vanity");
+    public static final ResourceLocation extract_item = new ResourceLocation(NABBA.MODID, "extract_item");
+    public static final ResourceLocation extract_fluid = new ResourceLocation(NABBA.MODID, "extract_fluid");
+    public static final ResourceLocation force_sync = new ResourceLocation(NABBA.MODID, "force_sync");
+    public static final ResourceLocation insert = new ResourceLocation(NABBA.MODID, "insert");
+    public static final ResourceLocation refresh_items = new ResourceLocation(NABBA.MODID, "refresh_items");
+    public static final ResourceLocation refresh_fluids = new ResourceLocation(NABBA.MODID, "refresh_fluids");
 
-    public static final Map<ResourceLocation,Consumer<FriendlyByteBuf>> serializers = new HashMap<>();
+    public static final Map<ResourceLocation, Consumer<FriendlyByteBuf>> serializers = new HashMap<>();
 
     public static void registerMessages() {
-        ServerPlayNetworking.registerGlobalReceiver(scroll,PacketHandler::receiveScroll);
-        ServerPlayNetworking.registerGlobalReceiver(scroll_keyring,PacketHandler::receiveScrollKeyring);
+        ServerPlayNetworking.registerGlobalReceiver(scroll, PacketHandler::receiveScroll);
+        ServerPlayNetworking.registerGlobalReceiver(scroll_keyring, PacketHandler::receiveScrollKeyring);
         ServerPlayNetworking.registerGlobalReceiver(search, PacketHandler::receiveSearch);
         ServerPlayNetworking.registerGlobalReceiver(vanity, PacketHandler::receiveVanity);
         ServerPlayNetworking.registerGlobalReceiver(extract_item, PacketHandler::receiveExtractItem);
         ServerPlayNetworking.registerGlobalReceiver(extract_fluid, PacketHandler::receiveExtractFluid);
         ServerPlayNetworking.registerGlobalReceiver(force_sync, PacketHandler::receiveForceRefresh);
-        ServerPlayNetworking.registerGlobalReceiver(insert,PacketHandler::receiveInsert);
+        ServerPlayNetworking.registerGlobalReceiver(insert, PacketHandler::receiveInsert);
     }
 
     public static void sendToServer(ResourceLocation packet, Consumer<FriendlyByteBuf> consumer) {
@@ -59,62 +60,88 @@ public class PacketHandler {
     public static void sendToClient(ServerPlayer player, ResourceLocation packet, Consumer<FriendlyByteBuf> consumer) {
         FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
         consumer.accept(buf);
-        ServerPlayNetworking.send(player,packet,buf);
+        ServerPlayNetworking.send(player, packet, buf);
     }
 
+    //note, all of these are called on the network thread, have to make sure to execute server code on the main thread
     private static void receiveScroll(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        AbstractContainerMenu container = player.containerMenu;
-        if (container instanceof SearchableMenu<?> antiBarrelMenu) {
-            antiBarrelMenu.handleScroll(player,buf.readInt());
-        }
+        int i = buf.readInt();
+        server.execute(() -> {
+            AbstractContainerMenu container = player.containerMenu;
+            if (container instanceof SearchableMenu<?> searchableMenu) {
+                searchableMenu.handleScroll(player, i);
+            }
+        });
     }
 
     private static void receiveScrollKeyring(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        if (player.getMainHandItem().getItem() instanceof KeyRingItem) {
-            CommonUtils.scrollKey(player.getMainHandItem(),buf.readBoolean());
-        }
+        boolean b = buf.readBoolean();
+        server.execute(() -> {
+            if (player.getMainHandItem().getItem() instanceof KeyRingItem) {
+                CommonUtils.scrollKey(player.getMainHandItem(), b);
+            }
+        });
     }
 
     private static void receiveSearch(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        AbstractContainerMenu container = player.containerMenu;
-        if (container instanceof SearchableItemMenu<?> antiBarrelMenu) {
-            antiBarrelMenu.handleSearch(player, buf.readUtf());//search
-        }
+        String search = buf.readUtf();
+        server.execute(() -> {
+            AbstractContainerMenu container = player.containerMenu;
+            if (container instanceof SearchableItemMenu<?> antiBarrelMenu) {
+                antiBarrelMenu.handleSearch(player, search);//search
+            }
+        });
     }
 
     private static void receiveVanity(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        AbstractContainerMenu container = player.containerMenu;
-        if (container instanceof VanityKeyMenu vanityKeyMenu) {
-            vanityKeyMenu.receiveVanity(buf.readInt(),buf.readDouble());//color, size
-        }
+        int color = buf.readInt();
+        double size = buf.readDouble();
+        server.execute(() -> {
+            AbstractContainerMenu container = player.containerMenu;
+            if (container instanceof VanityKeyMenu vanityKeyMenu) {
+                vanityKeyMenu.receiveVanity(color, size);//color, size
+            }
+        });
     }
 
     private static void receiveExtractItem(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        AbstractContainerMenu container = player.containerMenu;
-        if (container instanceof SearchableItemMenu<?> antiBarrelMenu) {
-            antiBarrelMenu.handleItemExtract(player,buf.readItem(), buf.readBoolean());//stack, shift
-        }
+        ItemStack stack = buf.readItem();
+        boolean shift = buf.readBoolean();
+        server.execute(() -> {
+            AbstractContainerMenu container = player.containerMenu;
+            if (container instanceof SearchableItemMenu<?> antiBarrelMenu) {
+                antiBarrelMenu.handleItemExtract(player, stack, shift);//stack, shift
+            }
+        });
     }
 
     private static void receiveExtractFluid(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        AbstractContainerMenu container = player.containerMenu;
-        if (container instanceof SearchableFluidMenu<?> antiBarrelMenu) {
-            FabricFluidStack fluidVariant = FabricFluidStack.fromPacket(buf);
-            antiBarrelMenu.handleFluidExtract(player,fluidVariant, buf.readBoolean());//stack, shift
-        }
+        FabricFluidStack fluidStack = FabricFluidStack.fromPacket(buf);
+        boolean shift = buf.readBoolean();
+        server.execute(() -> {
+            AbstractContainerMenu container = player.containerMenu;
+            if (container instanceof SearchableFluidMenu<?> antiBarrelMenu) {
+                antiBarrelMenu.handleFluidExtract(player, fluidStack, shift);//stack, shift
+            }
+        });
     }
 
     private static void receiveForceRefresh(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        AbstractContainerMenu container = player.containerMenu;
-        if (container instanceof SearchableMenu<?> antiBarrelMenu) {
-            antiBarrelMenu.refreshDisplay(player,true);
-        }
+        server.execute(() -> {
+            AbstractContainerMenu container = player.containerMenu;
+            if (container instanceof SearchableMenu<?> antiBarrelMenu) {
+                antiBarrelMenu.refreshDisplay(player, true);
+            }
+        });
     }
 
     private static void receiveInsert(MinecraftServer server, ServerPlayer player, ServerGamePacketListenerImpl handler, FriendlyByteBuf buf, PacketSender responseSender) {
-        AbstractContainerMenu container = player.containerMenu;
-        if (container instanceof SearchableMenu<?> searchableMenu) {
-            searchableMenu.handleInsert(player,buf.readInt());//count
-        }
+        int count = buf.readInt();
+        server.execute(() -> {
+            AbstractContainerMenu container = player.containerMenu;
+            if (container instanceof SearchableMenu<?> searchableMenu) {
+                searchableMenu.handleInsert(player,count);//count
+            }
+        });
     }
 }
