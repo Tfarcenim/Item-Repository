3da3710386c65f14590617398628eb9428610da2
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-10-03 23:11:33 -0400

    fixes so it actually builds

--- fabric/src/main/java/tfar/nabba/shim/IItemHandlerShim.java
+++ fabric/src/main/java/tfar/nabba/shim/IItemHandlerShim.java
@@ -41,7 +41,6 @@ public interface IItemHandlerShim {
      * Inserts an ItemStack into the given slot and return the remainder.
      * The ItemStack <em>should not</em> be modified in this function!
      * </p>
-     * Note: This behaviour is subtly different from {@link IFluidHandler#fill(FluidStack, IFluidHandler.FluidAction)}
      *
      * @param slot     Slot to insert into.
      * @param stack    ItemStack to insert. This must not be modified by the item handler.
